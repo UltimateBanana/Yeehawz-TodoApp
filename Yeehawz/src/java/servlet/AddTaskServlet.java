a6770c186ec229468c00034436b1743b2b75501e
IamKorby <justinkerrbie_96@yahoo.com>
2016-06-16 23:09:46 +0800

    Connected Create Task to Database (still has bugs)
    - cannot return the auto generated id from the database
    - not yet returning the created task to the UI

--- Yeehawz/src/java/servlet/AddTaskServlet.java
+++ Yeehawz/src/java/servlet/AddTaskServlet.java
@@ -8,6 +8,7 @@ package servlet;
 import controller.Controller;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Calendar;
 import javax.servlet.ServletException;
@@ -92,14 +93,22 @@ public class AddTaskServlet extends HttpServlet
 	String userId = request.getParameter(User.COLUMN_ID);
 	String title = request.getParameter(Task.COLUMN_TITLE);
 	String description = request.getParameter(Task.COLUMN_DESCRIPTION);
+	String schedule = request.getParameter(Task.COLUMN_SCHEDULE);
+	String[] date = schedule.split("/");
+	System.out.println(date[0] + " " + date[1] + " " + date[2]);
+	
 	Calendar calendar = Calendar.getInstance();
+	calendar.setTime(new Date(Integer.parseInt(date[2]) - 1900, Integer.parseInt(date[0]) - 1, Integer.parseInt(date[1])));
+	System.out.println("SHIT: " + calendar.get(Calendar.MONTH) + " " + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.YEAR));
+	
 	ArrayList<Tag> tagList = new ArrayList<>();
 	
 	Task task = new Task(title, description, calendar, tagList);
 	
 	Controller controller = new Controller();
 	
-	int id = controller.insertTask(userId, task);
+	//todo: change to userID when sessions are fixed
+	int id = controller.insertTask("1", task);
 	
 	if( id != -999 )
 	{
